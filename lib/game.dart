c00c0ca8f978fa415b1a4cfe352d3d1076ca28ee
namtrungle20 <121472249+namtrungle20@users.noreply.github.com>
2024-06-13 21:14:53 +0700

    first commit

---
+++ lib/game.dart
@@ -0,0 +1,95 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:bishop/bishop.dart' as bishop;
+import 'package:squares/squares.dart';
+import 'package:square_bishop/square_bishop.dart';
+
+
+class GamePage extends StatefulWidget {
+  // ignore: use_super_parameters
+  const GamePage({Key? key}) : super(key: key);
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  late bishop.Game game;
+  late SquaresState state;
+  int player = Squares.white;
+  bool aiThinking = false;
+  bool flipBoard = false;
+
+  @override
+  void initState() {
+    _resetGame(false);
+    super.initState();
+  }
+
+  void _resetGame([bool ss = true]) {
+    game = bishop.Game(variant: bishop.Variant.standard());
+    state = game.squaresState(player);
+    if (ss) setState(() {});
+  }
+
+  void _flipBoard() => setState(() => flipBoard = !flipBoard);
+
+  void _onMove(Move move) async {
+    bool result = game.makeSquaresMove(move);
+    if (result) {
+      setState(() => state = game.squaresState(player));
+    }
+    if (state.state == PlayState.theirTurn && !aiThinking) {
+      setState(() => aiThinking = true);
+      await Future.delayed(
+          Duration(milliseconds: Random().nextInt(4750) + 250));
+      game.makeRandomMove();
+      setState(() {
+        aiThinking = false;
+        state = game.squaresState(player);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Squares Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: BoardController(
+                state: flipBoard ? state.board.flipped() : state.board,
+                playState: state.state,
+                pieceSet: PieceSet.merida(),
+                theme: BoardTheme.brown,
+                moves: state.moves,
+                onMove: _onMove,
+                onPremove: _onMove,
+                markerTheme: MarkerTheme(
+                  empty: MarkerTheme.dot,
+                  piece: MarkerTheme.corners(),
+                ),
+                promotionBehaviour: PromotionBehaviour.autoPremove,
+              ),
+            ),
+            const SizedBox(height: 32),
+            OutlinedButton(
+              onPressed: _resetGame,
+              child: const Text('New Game'),
+            ),
+            IconButton(
+              onPressed: _flipBoard,
+              icon: const Icon(Icons.rotate_left),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
